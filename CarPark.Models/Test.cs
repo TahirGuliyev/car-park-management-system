3ef9562a0f907018f74340bb3bcf3b7d31716a64
Tahir Guliyev <tahirguliyev95@gmail.com>
2023-08-25 23:28:42 +0400

    MongoDB connection added

---
+++ CarPark.Models/Test.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Net.Sockets;
+
+namespace CarPark.Models
+{
+    public class Test
+    {
+        [BsonId]
+        public ObjectId _Id { get; set; }
+        public string NameSurname { get; set; }
+        public int Age { get; set; }
+        public ICollection<Address> Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
